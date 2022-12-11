2ad232ae07ec4d4cfcf0f7026bb7e200f9d115a6
Patrick Coston <patcoston@gmail.com>
2022-12-11 17:21:57 -0500

    Removed comments and white space

--- src/App.tsx
+++ src/App.tsx
@@ -10,42 +10,22 @@ function App() {
   const [undoStack, setUndoStack] = useState<Dot[]>([])
 
   const handleClick = (e: MouseEvent) => {
-    // get the coordinates of the click
     const x = e.clientX
     const y = e.clientY
-
-    // create a new dot at the click position
-    const dot = {
-      x,
-      y,
-    }
-
-    // add the new dot to the list of dots
+    const dot = { x, y }
     setDots([...dots, dot])
   }
 
   const handleUndo = () => {
-    // remove the last dot from the list of dots
     const newDots = dots.slice(0, -1)
-
-    // add the removed dot to the undo stack
     setUndoStack([...undoStack, dots[dots.length - 1]])
-
-    // update the list of dots
     setDots(newDots)
   }
 
   const handleRedo = () => {
-    // get the last dot from the undo stack
     const dot = undoStack[undoStack.length - 1]
-
-    // remove the dot from the undo stack
     const newUndoStack = undoStack.slice(0, -1)
-
-    // add the dot back to the list of dots
     setDots([...dots, dot])
-
-    // update the undo stack
     setUndoStack(newUndoStack)
   }
 
